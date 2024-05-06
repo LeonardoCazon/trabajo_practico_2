51bba01497d33e240969819694faa9290743ab2e
Leonardo Cazon <leofa@DESKTOP-KV5FD2M>
2024-05-06 02:46:56 -0300

    Ejercicio 7

---
+++ TP2_cazonLeonardo/src/ar/edu/unju/fi/ejercicio7/model/Categoria.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.ejercicio7.model;
+
+public enum Categoria {
+	TELEFONIA,
+	INFORMATICA,
+	ELECTROHOGAR,
+	HERRAMIENTAS
+}
---
+++ TP2_cazonLeonardo/src/ar/edu/unju/fi/ejercicio7/model/Origen.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.ejercicio7.model;
+
+public enum Origen {
+	ARGENTINA,
+	CHINA,
+	BRASIL,
+	URUGUAY
+}
